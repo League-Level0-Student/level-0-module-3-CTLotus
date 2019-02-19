9eed3250d7f81a849afe571713d1651958985dc0
60222 <60222@0338S-S-R0B6ZHH.sdcs.local>
2019-02-19 14:23:01 -0800

    2/19/19

--- src/methodwriting/ObedientRobot.java
+++ src/methodwriting/ObedientRobot.java
@@ -5,48 +5,60 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
-public static void main(String[] args) {
-	Robot x = new Robot();
-	x.setSpeed(100);
-	x.penDown();
-	
-	drawSquare();
-	for (int i = 0; i < 5; i++) {
+	static Robot x = new Robot();
+
+	public static void main(String[] args) {
+		{
+			x.setSpeed(100);
+			x.penDown();
+		}
+		String color = JOptionPane.showInputDialog(null, "Which of the three color do you want? Red? Blue? Green?");
+			if (color.equalsIgnoreCase("Red")) {
+				x.setPenColor(255,0,0);
+			}
+			if (color.equalsIgnoreCase("Blue")){
+				x.setPenColor(0,255,0);
+			}
+			if (color.equalsIgnoreCase("Green")) {
+				x.setPenColor(0,0,255);
+			}
+		String draw = JOptionPane.showInputDialog(null, "Which shape do you want to draw? Square? Triangle? Circle?");
+
+		if (draw.equalsIgnoreCase("Square")) {
+			drawSquare();
+		}
+		if (draw.equalsIgnoreCase("Triangle")) {
+			drawTriangle();
+		}
+		if (draw.equalsIgnoreCase("Circle")) {
+			drawCircle();
+		}
+	}
+
+	private static void drawCircle() {
+		for (int i = 0; i < 20; i++) {
+			x.turn(10);
+			x.move(10);
+			x.move(10);
+			x.turn(10);
+		}
+	}
+
+	private static void drawTriangle() {
+		x.turn(75);
+		x.move(100);
+		x.turn(-150);
+		x.move(100);
+		x.turn(-105);
 		x.move(50);
-		x.turn(90);
-		
-	}
-	
-	drawTriangle();
-	x.turn(75);
-	x.move(100);
-	x.turn(-150);
-	x.move(100);
-	x.turn(-105);
-	x.move(50);
-	
-	drawCircle();
-	for (int i = 0; i < 20; i++) {
-		x.turn(10);
-		x.move(10);
-		x.move(10);
-		x.turn(10);
-	}
-	int ask = JOptionPane.showOptionDialog(null, "What shape do you want?", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Sqaure", "Triangle", "Circle" }, null);};
-	
-	
-private static void drawCircle() {
-	// TODO Auto-generated method stub
-	
-}
 
-private static void drawTriangle() {
-	// TODO Auto-generated method stub
-	
-}
+	}
 
-private static void drawSquare() {
-	// TODO Auto-generated method stub
-	
-}
+	private static void drawSquare() {
+		for (int i = 0; i < 5; i++) {
+			x.move(50);
+			x.turn(90);
+
+		}
+	}
 }
